fbe501e8d880c339d9bb3d58e3d4c9132eaf5b0f
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-10-06 17:02:49 +0600

    group route done

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
-  
+
+route::group(['prefix' => 'account'], function () {
+
+    //route call account/nameee
+
+    route::get('/', function () {
+        return "homeeeeee";
+    });
+
+    route::get('/profile', function () {
+        return "profile";
+    });
+
+      route::get('/logout', function () {
+        return "logout";
+    });
+
+
+
+
+});
