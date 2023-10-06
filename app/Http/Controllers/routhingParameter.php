72c2d801fe0f777e02e6eb8c091967df0d5fd20c
Konok majamder <58282427+konok101@users.noreply.github.com>
2023-10-06 14:58:24 +0600

    route parameter pass done

---
+++ app/Http/Controllers/routhingParameter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class routhingParameter extends Controller
+{
+    function myName($myName){
+
+        return $myName;
+
+    }
+}
